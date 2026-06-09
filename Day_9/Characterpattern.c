440aba9e7aafe87877b6a70bc929b0001a184a26
Shreya Singh <innovateshreya@gmail.com>
2026-06-09 13:22:52 +0530

    Day_9 Uploaded

---
+++ Day_9/Characterpattern.c
@@ -0,0 +1,29 @@
+/*Write a program to Print repeated character pattern.
+A
+BB
+CCC
+DDDD
+EEEEE
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    int n;
+
+    printf("Enter number of rows: ");
+    scanf("%d", &n);
+
+    for(int i = 0; i < n; i++)
+    {
+        for(int j = 0; j <= i; j++)
+        {
+            printf("%c ", 'A' + i);
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
